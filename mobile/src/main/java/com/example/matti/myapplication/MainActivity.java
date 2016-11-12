c399221c7631161fc334cdd81fb13fe3f5cea396
AlessandroMorelli96 <amorelli96@gmail.com>
2016-11-12 23:13:07 +0100

    E stata aggiunta il metodo per calcolare le coordinate gps

--- mobile/src/main/java/com/example/matti/myapplication/MainActivity.java
+++ mobile/src/main/java/com/example/matti/myapplication/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.matti.myapplication;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import static java.lang.Math.round;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LocationListener locationListener = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Restituisce un vettore di due valori double il primo e latitudine e il secondo longitudine
+    public double[] coordinateGPS(View view) {
+
+        //location
+        double loc[] = new double[2];
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new MyLocationListener();
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Richiesta permessi", Toast.LENGTH_SHORT).show();
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+
+        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        Location location;
+
+        if (isGPSEnabled) {
+            if (locationManager != null) {
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                if (location != null) {
+                    loc[0] = round(location.getLatitude()*10000)/10000.0;
+                    loc[1] = round(location.getLongitude()*10000)/10000.0;
+                }
+            }
+        }
+        //Toast.makeText(this, "Help me at (" + String.valueOf(loc[0]) + ", " + String.valueOf(loc[1]) + ")", Toast.LENGTH_SHORT).show();
+
+        return loc;
     }
 }
---
+++ mobile/src/main/java/com/example/matti/myapplication/MyLocationListener.java
@@ -0,0 +1,31 @@
+package com.example.matti.myapplication;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by alessandromorelli on 12/11/16.
+ */
+
+public class MyLocationListener implements LocationListener {
+    @Override
+    public void onLocationChanged(Location loc) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
+}
